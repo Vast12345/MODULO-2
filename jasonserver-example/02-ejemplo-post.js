03812f96cd5723406abb0f74c67c791dd092e57a
EnzoLopezMendez <143453673+Vast12345@users.noreply.github.com>
2024-01-13 02:30:51 +0000

    JSON, Node.js, and Databases

---
+++ jasonserver-example/02-ejemplo-post.js
@@ -0,0 +1,16 @@
+fetch('http://localhost:3000/trainers', {
+    method: 'POST',
+    body: JSON.stringify({
+        "id": "3",
+        "nombres": "Yulieth",
+        "apellidos": "Rueda",
+        "especialidad": "Fullstack Python"
+
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((error) => console.error("Error!!!: "+error));
